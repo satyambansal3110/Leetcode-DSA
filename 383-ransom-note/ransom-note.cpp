ea2dae2727fb12b5f80afd4029fc9b9f836f78c4
Satyam Bansal <satyambansal3110@gmail.com>
2025-09-27 22:50:46 +0530

    Time: 8 ms (20.22%) | Memory: 11.8 MB (50.64%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> map;
+        for(char c:magazine){
+            map[c]++;
+        }
+        for(char c:ransomNote){
+            if(map[c]>0){
+                map[c]--;
+            }
+            else{
+                return false;
+            }
+
+        }
+        return true;
+        
+    }
+};
