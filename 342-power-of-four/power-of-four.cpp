f037d763756a8e744db68c5d96391df797077e9f
Satyam Bansal <satyambansal3110@gmail.com>
2025-10-13 16:36:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.64%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfFour(int n) {
         if (n <= 0) return false;
 
-        for (int i = 0; pow(4, i) <= n; i++) {
+        for (int i = 0; pow(4,i) <= n; i++) {
             if (pow(4, i) == n) {
                 return true;
             }
