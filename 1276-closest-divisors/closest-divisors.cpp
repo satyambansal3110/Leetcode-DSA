6cac1c484ddd99058fb3d4056fb2e7e838444082
Satyam Bansal <satyambansal3110@gmail.com>
2025-10-24 20:08:48 +0530

    Time: 6 ms (56.20%) | Memory: 8.1 MB (82.23%) - LeetSync

--- 1276-closest-divisors/closest-divisors.cpp
+++ 1276-closest-divisors/closest-divisors.cpp
@@ -12,20 +12,20 @@ public:
             if (a % i == 0) {
                 ans.push_back(i);
                 ans.push_back(a / i);
-                break; // ✅ stop after first (closest) pair
+                break; 
             }
         }
 
-        // For num + 2
+       
         for (int i = sqrt(c); i >= 1; i--) {
             if (c % i == 0) {
                 ap.push_back(i);
                 ap.push_back(c / i);
-                break; // ✅ stop after first (closest) pair
+                break; 
             }
         }
 
-        // Compare which pair is closer
+    
         if (abs(ans[0] - ans[1]) <= abs(ap[0] - ap[1])) {
             return ans;
         } else {
