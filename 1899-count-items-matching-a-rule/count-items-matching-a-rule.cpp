d39d8be660f8fe148c00a639ee9c0f25aa5a0516
Satyam Bansal <satyambansal3110@gmail.com>
2025-12-25 21:36:48 +0530

    Time: 3 ms (22.79%) | Memory: 36.2 MB (86.52%) - LeetSync

---
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int c=0;
+        int index;
+        if(ruleKey=="type") index=0;
+        else if(ruleKey=="color") index=1;
+        else index=2;
+        for(int i=0;i<items.size();i++){
+            
+            if(items[i][index]==ruleValue){
+            c++;
+            }
+            
+            
+        }
+        return c;
+    }
+};
